c4aa1226d3c965d0976f34a152f5adc138dfe9e1
Mark takeda <starwalker.0218@gmail.com>
2021-02-09 19:25:56 +0900

    add travis script

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -xve
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
+
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu18.04_desktop.git
+cd ./ros_setup_scripts_Ubuntu18.04_desktop
+bash ./step0.bash
+bash ./step1.bash
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+source /opt/ros/melodic/setup.bash
+catkin_init_workspace
+cd ~/catkin_init_workspace
+catkin_make
