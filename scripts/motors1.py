4628f393476b7adb27f07b4ad13942d37e9863ee
Mark takeda <starwalker.0218@gmail.com>
2021-02-14 01:04:21 +0900

    add motors

--- scripts/motors1.py
+++ scripts/motors1.py
@@ -9,8 +9,8 @@ class Motor():
         if not self.set_power(True): sys.exit(1)
 
         rospy.on_shutdown(self.set_power)
-        self.sub_raw = rospy.Subcriber('motor_raw', MotorFreqs, self.callback_raw_freq)
-        self.sub_cmd_vel =  rospy.Subcriber('cmd_vel', Twist, self.callback_cmd_vel)
+        self.sub_raw = rospy.Subscriber('motor_raw', MotorFreqs, self.callback_raw_freq)
+        self.sub_cmd_vel =  rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
         self.last_time = rospy.Time.now()
         self.using_cmd_vel = False
 
