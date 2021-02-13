0367e7c21b29a4bcda68dceacb9296df8469ab68
Mark takeda <starwalker.0218@gmail.com>
2021-02-14 00:38:14 +0900

    add motors

---
+++ test/travis_test_motors1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env Python
+#encoding: utf8
+import unittest, rostest
+import rosnode, rospy
+import time
+from pimouse_ros.msg import MotorsFreqs
+from geometry_msgs.msg imoort Twist
+
+class MotorTest(unittest.TestCase):
+    def file_check(self, dev, value, message):
+        with open("/dev/" + dev, "r") as f:
+            self.assertEqual(f.readline(), str(value)+"\n", message)
+
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('motors', node, "node does not exsit")
+
+    def test_put_freq(self):
+        pub = rospy.Publisher('/motor_raw', MotorsFreqs)
+        m = MotorsFreqs()
+        m.left_hz = 123
+        m.right_hz = 456
+        for i in range(10):
+            pub.publish(m)
+            time.sleep(0.1)
+
+        self.file_check("rtmotor_raw_l0", m.left_hz, "wromng left value from motor_raw")
+        self.file_check("rtmotor_raw_r0", m.right_hz, "wromng left value from motor_raw")
+
+    def test_put_cmd_el(self):
+        pub = rospy.Publisher('/cmd_vel',  Twist)
+        m = Twist()
+        m.linear.x = 0.1414
+        m.angular.z = 1.57
+        for i in range(10):
+            pub.publish(m)
+            time.sleep(0.1)
+
+        self.file_check("rtmotor_raw_l0", 200, "wromng left value from motor_raw")
+        self.file_check("rtmotor_raw_r0", 600, "wromng left value from motor_raw")
+
+        time.sleep(1.1)
+        self.file_check("rtmotor_raw_l0", 0, "don't stop after [1s]")
+        self.file_check("rtmotor_raw_r0", 0, "don't stop after [1s]")
+
+    if __name__ == '__main__':
+        time.sleep(3)
+        rospy.init_node('travis_test_motors')
+        rostest.rosrun('pimouse_ros', 'travis_test_motors',MotorTest)
