8b7ced63640909e729cd418f4d0b4b6ee9019343
Mark takeda <starwalker.0218@gmail.com>
2021-02-14 14:34:04 +0900

    add motor service

--- scripts/motors2.py
+++ scripts/motors2.py
@@ -55,7 +55,7 @@ class Motor():
     def onoff_response(self, onoff):
         d = TriggerResponse()
         d.success = self.set_power(onoff)
-        d.message = "ON" if self.is_on "OFF"
+        d.message = "ON" if self.is_on else "OFF"
         return d
 
     def callback_on(self,message):return self.onoff_response(True)
