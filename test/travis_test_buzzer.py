89ee2df282f65247e89c9c84e031a6ee0fcfbb2e
Mark takeda <starwalker.0218@gmail.com>
2021-02-14 22:02:07 +0900

    add muisc

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #encoding: utf8
-import rospy, unittest, rostest
+import rospy, unittest, rostest, actionlib
 import rosnode
 import time
 from std_msgs.msg import UInt16
-from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback
+from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal
 
 class BuzzerTest(unittest.TestCase):
     def setUp(self):
